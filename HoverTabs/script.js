6029d46c67bbd6e604aea7568197b92964da7a92
dragonbead95 <dragonbead95@naver.com>
2020-09-06 09:30:09 +0900

    W3schools HowToSection HoverTabs
    Vertical Tabs에서 mouseover 이벤트 활용

---
+++ HoverTabs/script.js
@@ -0,0 +1,29 @@
+const tablinks = document.querySelectorAll(".tablinks");
+function openCity(event, cityName) {
+  let i, tabcontent;
+
+  //tabcontent 박스들을 전부 제거
+  tabcontent = document.querySelectorAll(".tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+
+  // 클릭한 도시의 tabcontent의 display를 block으로 설정
+  city = document.querySelector("#" + cityName);
+  city.style.display = "block";
+  event.currentTarget.classList.add("active");
+}
+
+tablinks.forEach((item) => {
+  item.addEventListener(
+    "mouseover",
+    function (event) {
+      openCity(event, item.textContent);
+    },
+    false
+  );
+});
